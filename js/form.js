b08b98465e11ae60607eb1f6aedd525bfbe8a6b4
Ihor Chyzh <djar80@gmail.com>
2023-03-30 21:13:41 +0200

    adding hashtag validation

--- js/form.js
+++ js/form.js
@@ -4,6 +4,17 @@ const imageUpload = imageUploadForm.querySelector('.img-upload__overlay');
 const cancelUpload = imageUpload.querySelector('#upload-cancel');
 const hashtagField = imageUploadForm.querySelector('.text__hashtags');
 const descriptionField = imageUploadForm.querySelector('.text__description');
+const HASHTAG_PATTERN = /^#[a-zа-яё0-9]{1,19}$/i;
+const MAX_NUMBER_HASHTAG = 5;
+const ERRORE_INPUT_HASHTAG = 'Неправильный формат хэштэга';
+const ERRORE_INPUT_DESCRIPTION = 'Количество знаков не больше 140!';
+
+const pristine = new Pristine(imageUploadForm, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__field-wrapper--errore',
+});
+
 const closeUploadOnEscape = () => {
   document.removeEventListener('keydown', onEscapeDown);
 };
@@ -14,6 +25,7 @@ const closeUploadFile = () => {
     document.body.classList.remove('modal-open');
   });
   document.addEventListener('keydown', onEscapeDown);
+  imageUploadForm.reset();
 };
 
 uploadFile.addEventListener('change', (evt) => {
@@ -40,5 +52,24 @@ descriptionField.addEventListener('keydown', (evt) => {
     evt.stopPropagation();
   }
 });
+const validateDescription = (value) => value.length <= 140;
 
-
+const isValidTag = (tag) => HASHTAG_PATTERN.test(tag);
+const hasValidNumber = (tags) => tags.length <= MAX_NUMBER_HASHTAG;
+const hasUniquTag = (tags) => {
+  const lowerCaseTag = tags.map((tag) => tag.toLowerCase());
+  return lowerCaseTag.length === new Set(lowerCaseTag).size;
+};
+const validateTags = (value) => {
+  const tags = value
+    .trim()
+    .split(' ')
+    .filter((element) => element.trim().length);
+  return tags.every(isValidTag) && hasValidNumber(tags) && hasUniquTag(tags);
+};
+pristine.addValidator(descriptionField, validateDescription,ERRORE_INPUT_DESCRIPTION);
+pristine.addValidator(hashtagField, validateTags, ERRORE_INPUT_HASHTAG);
+imageUploadForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+});
