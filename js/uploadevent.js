6084788a52e6aee472d003b68371e03ed46d951e
Ihor Chyzh <djar80@gmail.com>
2023-04-06 17:21:11 +0200

    adding uploadevent.js

--- js/uploadevent.js
+++ js/uploadevent.js
@@ -2,21 +2,56 @@ const templateFragmentSuccess = document.querySelector('#success').content;
 const successModal = templateFragmentSuccess.querySelector('.success');
 const successButton = templateFragmentSuccess.querySelector('.success__button');
 const body = document.querySelector('body');
+
+const closeSuccess = () => {
+  successModal.remove();
+};
 const showSuccessUpload = () => {
   body.appendChild(successModal);
+  const closeNonActiveSuccess = (evt) => {
+    const target = evt.target;
+    if (!target.closest('.success__inner') && !target.closest('.success__button')) {
+      closeSuccess();
+    }
+  };
+  successButton.addEventListener('click', closeSuccess, {once: true});
+  document.addEventListener('keydown', onEscapeDownSuccess, true);
+  successModal.addEventListener('click',closeNonActiveSuccess);
 };
 
 const templateFragmentError = document.querySelector('#error').content;
 const errorModal = templateFragmentError.querySelector('.error');
+const errorButton = templateFragmentError.querySelector('.error__button');
+
+const closeError = () => {
+  errorModal.remove();
+};
+
 const showErrorUpload = () => {
   body.appendChild(errorModal);
+  const closeNonActiveError = (evt) => {
+    const target = evt.target;
+    if (!target.closest('.error__inner') && !target.closest('.error__button')) {
+      closeError();
+    }
+  };
+  errorButton.addEventListener('click', closeError, {once: true});
+  document.addEventListener('keydown', onEscapeDownErrore, true);
+  window.addEventListener('click', closeNonActiveError);
 };
 
 
-function onEscapeDown (evt) {
+function onEscapeDownSuccess (evt) {
   if (evt.key === 'Escape') {
-    closeModal();
-    closeUploadOnEscape();
+    document.removeEventListener('keydown', onEscapeDownSuccess, true);
+    closeSuccess();
+  }
+}
+function onEscapeDownErrore (evt) {
+  if (evt.key === 'Escape') {
+    evt.stopPropagation();
+    document.removeEventListener('keydown', onEscapeDownErrore, true);
+    closeError();
   }
 }
 export {showSuccessUpload, showErrorUpload};
